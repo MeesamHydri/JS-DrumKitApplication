bd607d67e8e6d98bd77c405f0b96606125aee188
meesamhydri <meesamhydri@gmail.com>
2025-09-06 17:58:55 +0500

    Complete Drum Kit

--- index.js
+++ index.js
@@ -1,14 +1,72 @@
-// document.querySelectorAll("button").addEventListener("click", handleClick);
 
-
-
-
+//Select all the buttons
 var allButtons = document.querySelectorAll("button");
 
+//loop all the buttons to add event listener to do action
 for (var i=0; i < allButtons.length; i++) {
-      document.querySelectorAll("button")[i].addEventListener("click", handleClick);
+      document.querySelectorAll("button")[i].addEventListener("click", function() {
+
+        var buttonText = this.innerHTML;
+        makeSound(buttonText);
+        buttonAnimation(buttonText);
+
+      });
 }
 
-function handleClick() {
-  alert("I got clicked!")
+//on keypress make sound and add button animation
+document.addEventListener("keydown", function (e) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+//function to make sounds
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio('sounds/tom-1.mp3');
+      audio.play();
+    break;
+
+    case "a":
+      var audio = new Audio('sounds/tom-2.mp3');
+      audio.play();
+    break;
+
+    case "s":
+      var audio = new Audio('sounds/tom-3.mp3');
+      audio.play();
+    break;
+
+    case "d":
+      var audio = new Audio('sounds/tom-4.mp3');
+      audio.play();
+    break;
+
+    case "j":
+      var audio = new Audio('sounds/snare.mp3');
+      audio.play();
+    break;
+
+    case "k":
+      var audio = new Audio('sounds/kick-bass.mp3');
+      audio.play();
+    break;
+
+    case "l":
+      var audio = new Audio('sounds/crash.mp3');
+      audio.play();
+    break;
+
+    default:
+  }
+
+};
+
+//Adding shadow animation on button
+
+function buttonAnimation(currentKey) {
+    var activeBtn = document.querySelector("."+currentKey);
+    activeBtn.classList.add("pressed");
+    setTimeout(function () {activeBtn.classList.remove("pressed")}, 100);
 }
